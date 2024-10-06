6bfee599a157b7c4ba2ac50424953eb66fc5a8e2
Kengo115 <bp21007@shibaura-it.ac.jp>
2024-10-06 11:00:50 +0900

    PhysarumSolverが動くようになった

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,4 +1,10 @@
 package client;
 
+import item.*;
 public class Client {
+    private Beacon Source;
+    private Beacon Distination;
+    private int TheNumberOfUAV;
+    private UAV[] uav;
+
 }
--- src/item/Beacon.java
+++ src/item/Beacon.java
@@ -1,4 +1,19 @@
 package item;
 
 public class Beacon {
+    private double x;
+    private double y;
+    //座標を設定する
+    public void setPos(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    //x座標を返す
+    public double getX() {
+        return x;
+    }
+    //y座標を返す
+    public double getY() {
+        return y;
+    }
 }
